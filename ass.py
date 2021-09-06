74e53cbb73adff1f2baf94897dbf221e593cce9c
Jeevitesh Man Shrestha <46070461+XxTyaftioNxX@users.noreply.github.com>
2021-09-06 17:21:16 +0545

    Delete ass.py

--- ass.py
+++
@@ -1,8 +0,0 @@
-import re
-ass = ['hello boi(nigga)', 'hello boi(nigga2)', 'hello boi(nigga3)', 'hello boi(nigga4)']
-pattern = r'\((.*?)\)'
-inner = []
-for one in ass:
-    word = (re.findall(pattern, one))
-    inner.append(word[0])
-print(inner)
